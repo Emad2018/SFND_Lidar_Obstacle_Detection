d4825f4196d1066283bfb145ba7368da46a956c0
Emad <a.emad02018@gmail.com>
2020-10-21 03:31:24 +0200

    Add the third lesson

--- src/environment.cpp
+++ src/environment.cpp
@@ -54,8 +54,25 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer) {
   std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr,
             pcl::PointCloud<pcl::PointXYZ>::Ptr>
       segmentCloud = point_Processor.SegmentPlane(point_cloud, 100, 0.2);
-  renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-  renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+  // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+  // renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1,
+  // 0));
+
+  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters =
+      point_Processor.Clustering(segmentCloud.first, 1.0, 3, 30);
+
+  int clusterId = 0;
+  std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)};
+
+  for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters) {
+    std::cout << "cluster size ";
+    point_Processor.numPoints(cluster);
+    renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId),
+                     colors[clusterId]);
+    Box box = point_Processor.BoundingBox(cluster);
+    renderBox(viewer, box, clusterId);
+    ++clusterId;
+  }
 }
 
 // setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -3,45 +3,102 @@
 
 #include "../../render/render.h"
 
-
 // Structure to represent node of kd tree
-struct Node
-{
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+struct Node {
+  std::vector<float> point;
+  int id;
+  Node *left;
+  Node *right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+  Node(std::vector<float> arr, int setId)
+      : point(arr), id(setId), left(NULL), right(NULL) {}
 };
 
-struct KdTree
-{
-	Node* root;
+struct KdTree {
+  Node *root;
 
-	KdTree()
-	: root(NULL)
-	{}
+  KdTree() : root(NULL) {}
+  void add(Node **src, uint depth, std::vector<float> point, int id) {
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+    if (*src == NULL) {
+      *src = new Node(point, id);
+    } else {
+      uint rimender = depth % 2;
 
-	}
+      if (point[rimender] < ((*src)->point[rimender])) {
+        add(&((*src)->left), depth + 1, point, id);
 
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
+      } else {
+        add(&((*src)->right), depth + 1, point, id);
+      }
+    }
+  }
+  void insert(std::vector<float> point, int id) {
+    // TODO: Fill in this function to insert a new point into the tree
+    // the function should create a new node and place correctly with in the
+    // root
+    add(&root, 0, point, id);
+  }
 
+  void find(Node **src, uint depth, float *high, float *low,
+            std::vector<int> *ids) {
+
+    uint rimender = depth % 2;
+
+    if (*src != NULL) {
+
+      /* std::cout << "(*src)->id " << (*src)->id << "\n";
+      std::cout << "point=(" << (*src)->point[0] << "," << (*src)->point[1]
+                << ")\n";
+      std::cout << "depth= " << depth << "\n"; */
+
+      if (low[rimender] > ((*src)->point[rimender])) {
+        // std::cout << "a\n";
+        find(&((*src)->right), depth + 1, high, low, ids);
+
+      } else if (high[rimender] < ((*src)->point[rimender])) {
+        // std::cout << "b\n";
+        find(&((*src)->left), depth + 1, high, low, ids);
+      } else {
+        // std::cout << "c\n";
+        if ((low[!rimender] < ((*src)->point[!rimender])) &&
+            (high[!rimender] > ((*src)->point[!rimender]))) {
+          ids->push_back((*src)->id);
+        }
+
+        find(&((*src)->left), depth + 1, high, low, ids);
+        find(&((*src)->right), depth + 1, high, low, ids);
+      }
+    }
+  }
+  // return a list of point ids in the tree that are within distance of target
+  std::vector<int> search(std::vector<float> target, float distanceTol) {
+    std::vector<int> ids;
+
+    uint depth = 0;
+    float high[2] = {target[0] + distanceTol, target[1] + distanceTol};
+    float low[2] = {target[0] - distanceTol, target[1] - distanceTol};
+    // std::cout << "high=(" << high[0] << "," << high[1] << ")\n";
+    // std::cout << "low=(" << low[0] << "," << low[1] << ")\n";
+    find(&root, 0, high, low, &ids);
+    return ids;
+  }
+
+  /* extra functions to print the Tree elements and the relativity  */
+  void loop(Node **src) {
+    if (*src != NULL) {
+      std::cout << "(*src)->id " << (*src)->id << "\n";
+      std::cout << "point=(" << (*src)->point[0] << "," << (*src)->point[1]
+                << ")\n";
+      if ((*src)->left != NULL) {
+        std::cout << "left node of" << (*src)->id << ":\n";
+        loop(&(*src)->left);
+      }
+      if ((*src)->right != NULL) {
+        std::cout << "right node of" << (*src)->id << ":\n";
+        loop(&(*src)->right);
+      }
+    }
+  }
+  void print_nodes_information() { loop(&root); }
 };
-
-
-
-
